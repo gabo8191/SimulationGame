f25a7a34f39c52846a3c663592038e5860842cdb
brayooo <bryan.lopez@uptc.edu.co>
2024-05-25 16:25:03 -0500

    se arreglan vidas

--- Assets/Scripts/HUD.cs
+++ Assets/Scripts/HUD.cs
@@ -19,15 +19,31 @@ public class HUD : MonoBehaviour
         txtPuntos.text = "Puntos: " + puntos.ToString();
     }
 
-    public void desactivarVida(int vida)
+    public void desactivarVida(int vidasRestantes)
     {
-        vidas[vida].SetActive(false);
-        
+        // Asegurar que todos los corazones están activos antes de desactivar los necesarios
+        foreach (GameObject corazon in vidas)
+        {
+            corazon.SetActive(true);
+        }
+
+        // Desactivar corazones desde el primero hasta el último, basado en las vidas restantes
+        for (int i = vidas.Length - 1; i >= vidasRestantes; i--)
+        {
+            if (i >= 0 && i < vidas.Length)
+            {
+                vidas[i].SetActive(false);
+            }
+        }
     }
 
-    public void activarVida(int vida)
+    public void activarVida(int vidasActuales)
     {
-        vidas[vida].SetActive(true);
+        // Activar el corazón correspondiente a la próxima vida recuperada
+        if (vidasActuales < vidas.Length)
+        {
+            vidas[vidasActuales - 1].SetActive(true);
+        }
     }
 
 }
